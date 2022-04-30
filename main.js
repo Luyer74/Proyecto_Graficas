894b449a62719e44579aa0883b4569e54c9f73f2
Luis Yerik Arambula <A00825080@itesm.mx>
2022-04-30 18:11:05 -0500

    agregar pato

--- main.js
+++ main.js
@@ -1,45 +1,117 @@
 import "./style.css";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
-const scene = new THREE.Scene();
+//Loader
 const loader = new GLTFLoader();
+// Canvas
+const canvas = document.querySelector("canvas#bg");
 
+// Scene
+const scene = new THREE.Scene();
+
+// Objects
+
+// Materials
+
+// Mesh
+
+// Lights
+
+const pointLight = new THREE.PointLight(0xffffff, 0.8);
+pointLight.position.x = 2;
+pointLight.position.y = 3;
+pointLight.position.z = 4;
+scene.add(pointLight);
+
+// const pointLight2 = new THREE.PointLight(0xff0000, 0.2);
+// pointLight2.position.set(1, 1, 1);
+// pointLight2.intensity = 1;
+// scene.add(pointLight2);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.x = 0;
+camera.position.y = 0;
+camera.position.z = 2;
+scene.add(camera);
+
+// Controls
+// const controls = new OrbitControls(camera, canvas)
+// controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  alpha: true,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Animate
+ */
+
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update objects
+
+  // Update Orbital Controls
+  // controls.update()
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+//load
 loader.load(
-  "scene.gltf",
+  "./assets/duck/scene.gltf",
   function (gltf) {
     scene.add(gltf.scene);
+    gltf.scene.scale.set(0.4, 0.4, 0.4);
+    tick();
   },
   undefined,
   function (error) {
     console.error(error);
   }
 );
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
-camera.position.z = 2;
-
-function animate() {
-  requestAnimationFrame(animate);
-
-  cube.rotation.x += 0.02;
-  cube.rotation.y += 0.02;
-
-  renderer.render(scene, camera);
-}
-
-animate();
